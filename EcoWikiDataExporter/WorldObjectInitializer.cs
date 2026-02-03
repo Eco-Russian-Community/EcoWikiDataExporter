a17b49910fcff6328bc57da72d8ba5ec41c7662b
StalEF <driving-kovrov@yandex.ru>
2026-02-03 08:53:29 +0300

    add User for spawn WorldObjects

--- EcoWikiDataExporter/WorldObjectInitializer.cs
+++ EcoWikiDataExporter/WorldObjectInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Eco.Gameplay.Items;
+﻿using Eco.Gameplay.Utils;
+using Eco.Gameplay.Items;
 using Eco.Gameplay.Players;
 using Eco.Gameplay.Objects;
 using Eco.Shared.Logging;
@@ -7,21 +8,21 @@ using System;
 using System.Numerics;
 using Quaternion = Eco.Shared.Math.Quaternion;
 
+
 namespace Eco.Mods.EcoWikiDataExporter
 {
 	internal class WorldObjectInitializer
 	{
-        private static User dummy;
         static Vector3i spawnPoint = new Vector3i(0, 75, 0);
         public WorldObject? Init(WorldObjectItem item)
 		{
 			ArgumentNullException.ThrowIfNull(item);
 			Type worldObjectType = item.WorldObjectType;
-			//WorldObject worldObject = (WorldObject)Activator.CreateInstance(worldObjectType, true) ?? throw new Exception($"Error create instance of worldObject: {worldObjectType}");
-			
-			try
+			var WikiUser = TestUtils.TestUser;
+
+            try
 			{
-				WorldObject worldObject = WorldObjectManager.ForceAdd(worldObjectType, dummy, spawnPoint, Quaternion.Identity, false);
+				WorldObject worldObject = WorldObjectManager.ForceAdd(worldObjectType, WikiUser, spawnPoint, Quaternion.Identity, false);
 				worldObject.DoInitializationSteps();
 				return worldObject;
 			}
