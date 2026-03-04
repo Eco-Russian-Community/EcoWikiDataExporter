547e983ffbf42e5111f249624335f051725b569e
StalEF <driving-kovrov@yandex.ru>
2026-03-04 10:00:35 +0300

    Commands Table update
    Update code and add wiki Table style as on other pages

--- EcoWiki_Lua_Modules/CommandTable.lua
+++ EcoWiki_Lua_Modules/CommandTable.lua
@@ -4,60 +4,62 @@ local Utils = require('Module:Utils')
 local Lang = Utils.getLanguageName()
 
 function p.main()
-	local wiki = ''
+	local WikiText = ''
+	local TableHeader = ''
 	local wikiuser = ''
 	local wikiadmin = ''
 	local wikidev = ''
 
 	-- import the required modules
-	local commandsData = mw.loadData( "Module:CommandData" )	
+	local CommandData = mw.loadData( "Module:CommandData" )	
+	local Commands = CommandData.commands
+
+	-- Create the header of the table
+	TableHeader = '<table class="table table-striped table-bordered sortable"><tr class="thead-dark">' 
+	TableHeader = TableHeader .. '<th>Access<br>level</th>'
+	TableHeader = TableHeader .. '<th>Command</th>'
+	TableHeader = TableHeader .. '<th>Short<br>call</th>'
+	TableHeader = TableHeader .. '<th class="unsortable">Description<br>Arguments</th>'
 	
-	local commands = commandsData.commands
+	WikiText = TableHeader
 
-	-- create the header of the table
-	local header = "<table class=\"wikitable sortable\"> \n " 
-	header = header .. "<tr> \n "
-	header = header .. "<th>Access<br>level</th> \n "
-	header = header .. "<th>Command</th> \n "
-	header = header .. "<th>Short<br>call</th> \n "
-	header = header .. "<th>Description<br>Arguments</th> \n "
-	header = header .. "</tr> \n "
-	
-	wiki = header
+	for CommandName,CommandData in pairs(Commands) do
+		local TableRow = ''
+		local CommandDescription = ''
+		local CommandParameters = ''
+		local CommandParent = ''
+		local CommandShortCut = ''
+		TableRow = TableRow .. '<tr>'
 
-	for k,v in pairs(commands) do
-		local row = ''
-		row = row .. "<tr> \n "
-
-		if (v.parent == nil) then v.parent = '' else v.parent = v.parent .. " " end
-		if (v.parameters == nil) then v.parameters= '-'
+		if (CommandData.parent == nil) then CommandParent = '' else CommandParent = CommandData.parent .. " " end
+		if (CommandData.parameters == nil) then CommandParameters = '-'
 		else
-			if (v.parameters.Arg1 ~= nil) then ARG1 = "<br><b>" .. v.parameters.Arg1[1] .. "</b> (" .. v.parameters.Arg1[2] .. ")" else ARG1 = "" end
-			if (v.parameters.Arg2 ~= nil) then ARG2 = ", <b>" .. v.parameters.Arg2[1] .. "</b> (" .. v.parameters.Arg2[2] .. ")" else ARG2 = "" end
-			if (v.parameters.Arg3 ~= nil) then ARG3 = ", <b>" .. v.parameters.Arg3[1] .. "</b> (" .. v.parameters.Arg3[2] .. ")" else ARG3 = "" end
-			if (v.parameters.Arg4 ~= nil) then ARG4 = ", <b>" .. v.parameters.Arg4[1] .. "</b> (" .. v.parameters.Arg4[2] .. ")" else ARG4 = "" end
-			if (v.parameters.Arg5 ~= nil) then ARG5 = ", <b>" .. v.parameters.Arg5[1] .. "</b> (" .. v.parameters.Arg5[2] .. ")" else ARG5 = "" end
-			if (v.parameters.Arg6 ~= nil) then ARG6 = ", <b>" .. v.parameters.Arg6[1] .. "</b> (" .. v.parameters.Arg6[2] .. ")" else ARG6 = "" end
-			v.parameters= ARG1 .. ARG2 .. ARG3 .. ARG4 .. ARG5 .. ARG6
+			if (CommandData.parameters.Arg1 ~= nil) then ARG1 = '<br><b>' .. CommandData.parameters.Arg1[1] .. '</b> (' .. CommandData.parameters.Arg1[2] .. ')' else ARG1 = '' end
+			if (CommandData.parameters.Arg2 ~= nil) then ARG2 = ', <b>' .. CommandData.parameters.Arg2[1] .. '</b> (' .. CommandData.parameters.Arg2[2] .. ')' else ARG2 = '' end
+			if (CommandData.parameters.Arg3 ~= nil) then ARG3 = ', <b>' .. CommandData.parameters.Arg3[1] .. '</b> (' .. CommandData.parameters.Arg3[2] .. ')' else ARG3 = '' end
+			if (CommandData.parameters.Arg4 ~= nil) then ARG4 = ', <b>' .. CommandData.parameters.Arg4[1] .. '</b> (' .. CommandData.parameters.Arg4[2] .. ')' else ARG4 = '' end
+			if (CommandData.parameters.Arg5 ~= nil) then ARG5 = ', <b>' .. CommandData.parameters.Arg5[1] .. '</b> (' .. CommandData.parameters.Arg5[2] .. ')' else ARG5 = '' end
+			if (CommandData.parameters.Arg6 ~= nil) then ARG6 = ', <b>' .. CommandData.parameters.Arg6[1] .. '</b> (' .. CommandData.parameters.Arg6[2] .. ')' else ARG6 = '' end
+			CommandParameters = ARG1 .. ARG2 .. ARG3 .. ARG4 .. ARG5 .. ARG6
 		end
 
-		if (v.shortCut ~= '') then v.shortCut = "/".. v.shortCut end
+		if (CommandData.shortCut ~= '') then CommandShortCut = '/' .. CommandData.shortCut else CommandShortCut = '' end
 
-		row = row .. "<td>" .. v.level.. " </td> \n"
-		row = row .. "<td><b>/" .. v.parent .. v.command.. "</b></td> \n"
-		row = row .. "<td><b>" .. v.shortCut .. " </td> \n"
-		if (v.helpText[Lang] == '') then CommandDescription = v.helpText.English else CommandDescription = v.helpText[Lang] end
-		row = row .. "<td>" .. CommandDescription .. v.parameters .. "</b></td> \n"
+		TableRow = TableRow .. '<td>' .. CommandData.level.. ' </td>'
+		TableRow = TableRow .. '<td><b>/' .. CommandParent .. CommandData.command.. '</b></td>'
+		TableRow = TableRow .. '<td><b>' .. CommandShortCut .. ' </td>'
+		if (CommandData.helpText[Lang] == '') then CommandDescription = CommandData.helpText.English else CommandDescription = CommandData.helpText[Lang] end
+		TableRow = TableRow .. '<td>' .. CommandDescription .. CommandParameters .. '</b></td>'
 
-		row = row .. "</tr>"
-	if ( v.level == 'User' ) then wikiuser = wikiuser .. row end
-	if ( v.level == 'Admin' ) then wikiadmin = wikiadmin .. row end
-	if ( v.level == 'DevTier' ) then wikidev = wikidev .. row end
+		TableRow = TableRow .. '</tr>'
 
+		if ( CommandData.level == 'User' ) then wikiuser = wikiuser .. TableRow end
+		if ( CommandData.level == 'Admin' ) then wikiadmin = wikiadmin .. TableRow end
+		if ( CommandData.level == 'DevTier' ) then wikidev = wikidev .. TableRow end
 	end
-	wiki = wiki .. wikiuser .. wikiadmin .. wikidev
-	wiki = wiki .. " </table>"
-	return wiki
+	WikiText = WikiText .. wikiuser .. wikiadmin .. wikidev
+	WikiText = WikiText .. ' </table>'
+	return WikiText
 	end
 
 return p
