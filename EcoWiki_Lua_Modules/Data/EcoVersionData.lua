1e0de4d6d343168a16e9d5a31cfaf8870459fa88
StalEF <driving-kovrov@yandex.ru>
2026-05-09 16:28:51 +0300

    Update EcoVersionData.lua

--- EcoWiki_Lua_Modules/Data/EcoVersionData.lua
+++ EcoWiki_Lua_Modules/Data/EcoVersionData.lua
@@ -1,13 +1,13 @@
--- Eco Version : 0.13.0.2
--- EWDE Version : 0.7.1.0
--- Date of export : 16.04.2026 15:06:31
+-- Eco Version : 0.13.0.3
+-- EWDE Version : 0.7.1.1
+-- Date of export : 09.05.2026 10:52:38
 
 return {
     game = {
         ['eco'] = {
-                    ['Version'] = '0.13.0.2 beta release-1006',
-                    ['VersionNumber'] = '0.13.0.2',
-                    ['FullInfo'] = '0.13.0.2 beta release-1006 2026.04.15 19:38',
+                    ['Version'] = '0.13.0.3 beta release-1015',
+                    ['VersionNumber'] = '0.13.0.3',
+                    ['FullInfo'] = '0.13.0.3 beta release-1015 2026.05.04 16:52',
         },
     },
 }
